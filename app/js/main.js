f86c5da7f1f4f4a88e269f9d9d617fbac22cec13
Yury <yurykoropenko@gmail.com>
2017-03-01 16:50:12 +0200

    commit 3

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,12 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+	/**/
+	$('.nav__button').on('click', function() {
+		$('.header .nav').slideToggle();
+		$(document).click(function(event) {
+			if ($(event.target).closest('.header .nav').length) return;
+				$('.header .nav').slideUp();
+				event.stopPropagation();
+			});
+		return false;
+	});
+});
